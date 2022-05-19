e55c04237cacffe76f0cbd6489f8b9c3ceedd9c4
Fegsilva95 <12787529607@estudante.ufjf.br>
2022-05-18 22:42:27 -0300

    change labels hint

--- app/src/main/java/com/example/mystarter/MainActivity.java
+++ app/src/main/java/com/example/mystarter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mystarter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClickMessage(View origin) {
         TextView viewMessage = findViewById(R.id.viewMessage);
-        viewMessage.setText("Hello ");
+
+        EditText editTextName = findViewById(R.id.NameInput);
+        EditText editTextStarter = findViewById(R.id.starterInput);
+
+        String name = editTextName.getText().toString();
+        String starter = editTextStarter.getText().toString();
+        String message = String.format("Hi %s! Your starter is %s!", name, starter);
+
+        viewMessage.setText(message);
     }
 }
